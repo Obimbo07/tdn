10f3ee60fd10ea2286433e1b3a075ba3efe17866
Obimbo07 <obimboausts@gmail.com>
2024-01-10 12:55:47 +0300

    created-model

---
+++ db/migrate/20240110091245_add_user_and_post_ref_to_comments.rb
@@ -0,0 +1,6 @@
+class AddUserAndPostRefToComments < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :comments, :posts, null: false, foreign_key: true
+    add_reference :comments, :users, null: false, foreign_key: true
+  end
+end
---
+++ db/migrate/20240110094349_add_user_and_post_ref_to_likes.rb
@@ -0,0 +1,7 @@
+class AddUserAndPostRefToLikes < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :likes, :user, null: false, foreign_key: true
+    add_reference :likes, :posts, null: false, foreign_key: true
+
+  end
+end
