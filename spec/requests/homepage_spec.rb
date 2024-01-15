23d12fae0668b6cd3bb4ab849d7210b64b1666b7
Obimbo07 <obimboausts@gmail.com>
2024-01-15 18:42:39 +0300

    linters-fix

--- spec/requests/homepage_spec.rb
+++ spec/requests/homepage_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Homepages", type: :request do
-  describe "GET /hello" do
-    it "returns http success" do
-      get "/homepage/hello"
+RSpec.describe 'Homepages', type: :request do
+  describe 'GET /hello' do
+    it 'returns http success' do
+      get '/homepage/hello'
       expect(response).to have_http_status(:success)
     end
   end
