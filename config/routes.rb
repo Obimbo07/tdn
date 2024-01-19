51cd4e928d7417a14de14edb957c585d9b5147bc
Obimbo07 <obimboausts@gmail.com>
2024-01-19 18:53:06 +0300

    form-routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: [:show, :index] do
-    resources :posts, only: [:show, :index]
+    resources :posts, only: [:show, :index, :new, :create]
   end
 end
