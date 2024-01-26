b222d7d750c6fd124ade56fc3cff3378d62ad984
Obimbo07 <obimboausts@gmail.com>
2024-01-26 15:04:11 +0300

    whitespace-fix

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -5,10 +5,10 @@ class CommentsController < ApplicationController
     store_referer
     @comment = Comment.new
   end
-  
+
   def create
     @comment = @post.comments.new(comment_params.merge(user_id: current_user.id))
-  
+
     if @comment.save
       redirect_back_or_default(user_post_comments_path(@user, @post))
     else
@@ -21,16 +21,16 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text)
   end
-  
+
   def set_user_and_post
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:post_id])
   end
-  
+
   def store_referer
     session[:referer] = request.referer
   end
-  
+
   def redirect_back_or_default(default)
     redirect_to(session[:referer] || default)
     session.delete(:referer)
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,16 +8,16 @@ class LikesController < ApplicationController
   def create
     @post = @user.posts.find(params[:post_id])
     @like = @post.likes.new(user: current_user)
-  
+
     if @like.save
       redirect_to user_post_path(@user, @post), notice: 'Like was successfully created.'
     else
       render :new
     end
   end
-  
-    private
-  
+
+  private
+
   def set_user_and_post
     @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
