a05a91332373838523ddd734b1bed25dd687b278
Obimbo07 <obimboausts@gmail.com>
2024-01-26 14:59:54 +0300

    rubocop-fix

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   before_action :set_user_and_post, only: %i[new create]
 
-  def new 
+  def new
     store_referer
     @comment = Comment.new
   end
@@ -11,28 +11,28 @@ class CommentsController < ApplicationController
   
     if @comment.save
       redirect_back_or_default(user_post_comments_path(@user, @post))
-      else
-        render :new
-      end
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
   
-  private
+  def set_user_and_post
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+  end
   
-    def comment_params
-      params.require(:comment).permit(:text)
-    end
+  def store_referer
+    session[:referer] = request.referer
+  end
   
-    def set_user_and_post
-      @user = User.find(params[:user_id])
-      @post = @user.posts.find(params[:post_id])
-    end
-  
-    def store_referer
-      session[:referer] = request.referer
-    end
-  
-    def redirect_back_or_default(default)
-      redirect_to(session[:referer] || default)
-      session.delete(:referer)
-    end
+  def redirect_back_or_default(default)
+    redirect_to(session[:referer] || default)
+    session.delete(:referer)
+  end
 end
