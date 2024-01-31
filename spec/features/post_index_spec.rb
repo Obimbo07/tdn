73b80eff8238c52259d74fe82d7c6f35f9653a1c
Obimbo07 <obimboausts@gmail.com>
2024-02-01 01:28:37 +0300

    first 3 post spec added

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.feature 'Post Index View', type: :feature do
   scenario 'User views post index page' do
-    user = User.create(name: 'Austin', photo: 'https://example', bio: 'Intergration test user', posts_counter: 1)
-    Post.create(author: user, title: 'Hi', text: 'First Post', comment_counter: 1, like_counter: 3)
+    user = User.create(name: 'Austin', photo: 'https://example', bio: 'Integration test user', posts_counter: 0 )
+    post = Post.create(author: user, title: 'Hi', text: 'First Post', comment_counter: 1, like_counter: 3)
+
     visit user_posts_path(user)
 
     expect(page).to have_selector('.user_info')
@@ -12,16 +13,17 @@ RSpec.feature 'Post Index View', type: :feature do
     expect(page).to have_content("Number of posts: #{user.posts.count}")
 
     expect(page).to have_selector('.post', count: 1)
-    expect(page).to have_link('Post', href: user_post_path(user, user.posts.first))
-    expect(page).to have_content(user.posts.first.text)
-    expect(page).to have_css('.count', text: "Comments: #{user.posts.first.comments.count}")
-    expect(page).to have_css('.count', text: "Likes: #{user.posts.first.likes.count}")
+    expect(page).to have_content(post.title)
+    expect(page).to have_content(post.text)
+    expect(page).to have_css('.count', text: "Comments: #{post.comments.count}")
+    expect(page).to have_css('.count', text: "Likes: #{post.likes.count}")
 
     expect(page).to have_selector('.comments', count: 1)
-    user.posts.first.recent_comments.each do |comment|
+    post.recent_comments.each do |comment|
       expect(page).to have_content("#{comment.user.name} : #{comment.text}")
     end
 
     expect(page).to have_link('Pagination', href: user_posts_path(user))
+    expect(page).to have_link('Add New post', href: new_user_post_path(user))
   end
 end
