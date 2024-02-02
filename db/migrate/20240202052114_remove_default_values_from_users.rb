df5bde509c6840f72b312e6d9bda2155c5a89db6
Obimbo07 <obimboausts@gmail.com>
2024-02-02 08:26:11 +0300

    updated email and password default values

---
+++ db/migrate/20240202052114_remove_default_values_from_users.rb
@@ -0,0 +1,6 @@
+class RemoveDefaultValuesFromUsers < ActiveRecord::Migration[7.1]
+  def change
+    change_column_default :users, :email, nil
+    change_column_default :users, :encrypted_password, nil
+  end
+end
