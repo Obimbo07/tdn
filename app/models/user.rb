014228c2f202d91b1c14e26a914758eed9604fc5
izebiafe <afimiafidelis95@gmail.com>
2024-02-03 11:08:49 +0100

    add more authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@
   validates :email, presence: true, uniqueness: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   enum role = { user: 'user'}
+  def is?(requested_role)
+    role == requested_role.to_s
+  end
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
