68e398bb20d193d82624cb2800a416bd85dbedd2
Evans kofi Nyamekye <nyamekye2131@gmail.com>
2024-02-03 21:22:05 +0000

    Update posts_controller.rb

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,14 @@
+ class PostsController < ApplicationController
+        def index
+          user = User.find(params[:user_id])
+          posts = user.posts.includes(:comments, :likes)
+          render json: posts
+        end
+        def show
+          user = User.find(params[:user_id])
+          post = user.posts.includes(:comments, :likes).find(params[:id])
+          render json: post
+        end
+      end
+    end
+  end
