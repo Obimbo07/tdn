398ddad8f2ae16da89b82c45e391e56948e77f1a
izebiafe <afimiafidelis95@gmail.com>
2024-02-03 11:06:03 +0100

    add user authentication

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+  class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  enum role = { user: 'user'}
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
+end
