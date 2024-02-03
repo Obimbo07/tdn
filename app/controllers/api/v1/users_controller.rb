a9ee9c36ebce04af73a3534e2e83b3011a0ac926
Obimbo07 <obimboausts@gmail.com>
2024-02-03 23:55:51 +0300

    added routes and created api controllers

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,16 @@
+module Api
+    module V1
+      class UsersController < ApplicationController
+        def index
+          users = User.all
+          render json: users
+        end
+  
+        def show
+          user = User.includes(posts: [:comments, :likes]).find(params[:id])
+          render json: user
+        end
+      end
+    end
+  end
+  
